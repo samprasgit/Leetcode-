5e946dbcb666079e30879e38936720353bf67fb3
samprasyuan <14274044@bjtu.edu.cn>
2023-10-28 01:12:32 +0800

    update dfs notes

--- 深度优先搜索/堆栈实现深度优先搜索.py
+++ 深度优先搜索/堆栈实现深度优先搜索.py
@@ -4,11 +4,26 @@
 
 class Solution:
     def dfs_stack(self, graph, u):
+        """
+        graph   存储无向图的嵌套数组变量
+        u 当前起始点
+        visited
+
+        """
+        # visited 存放访问过的节点
+        # stack 用来存放节点访问记录
         visited, stack = set(), []
-        stack.append(u)
+
+        stack.append([u, 0])  # 节点u，以及节点u的下一邻接节点放入栈中，下次将遍历graph[u,0]
+        visited.add(u)  # 起始节点标记为已访问
+
         while stack:
-            u = stack.pop()
-            if u not in visited:
-                visited.add(u)
-                stack.extend(graph[u])
-        return visited
+            u, i = stack.pop()  # 取出节点u以及下一要访问的节点坐标i
+
+            if i < len(graph[u]):
+                v = graph[u][i]  # 取出邻接点
+                stack.pop([u, i + 1])  # 下一次将遍历 graph[u][i+1]
+                if v not in visited:
+                    print(v)
+                    stack.append([v, 0])  # 下一次将遍历graph[v][0]
+                    visited.add(v)  # 节点V标记为已访问
--- 深度优先搜索/递归实现深度优先搜索.py
+++ 深度优先搜索/递归实现深度优先搜索.py
@@ -3,6 +3,11 @@
 
 
 def dfs_recursive(graph, u, visited):
+    """
+    graph 存储无向图的嵌套数组变量
+    u 当前遍历边的开始节点
+    visited 标记访问节点的集合
+    """
     # 节点U已经访问
     visited.add(u)
 
@@ -11,3 +16,10 @@ def dfs_recursive(graph, u, visited):
         if v not in visited:
             # 深度优先遍历搜索
             dfs_recursive(graph, v, visited)
+
+
+graph = {"A": ["B", "C"], "B": ["A", "C", "D"], "C": ["A", "B", "D", "E"], "D": ["B", "C", "E", "F"], "E": ["C", "D"], "F": ["D", "G"], "G": []}
+
+# 基于递归实现的深度优先搜索
+visited = set()
+print(dfs_recursive(graph, "A", visited))
